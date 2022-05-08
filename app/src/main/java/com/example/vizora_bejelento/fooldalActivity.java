cdd5ec56f1370aed7c43eb3e111611997eb23ee8
Flora <florafovenyes09@gmail.com>
2022-05-08 22:41:08 +0200

    vizora

---
+++ app/src/main/java/com/example/vizora_bejelento/fooldalActivity.java
@@ -0,0 +1,17 @@
+package com.example.vizora_bejelento;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class fooldalActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fooldal);
+    }
+
+    public void updateAlertIcon() {
+    }
+}
